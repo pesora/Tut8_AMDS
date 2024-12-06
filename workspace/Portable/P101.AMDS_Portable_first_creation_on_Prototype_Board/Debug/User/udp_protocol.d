42ae972268c013c9df39462f70a7e98d6393c227
pesora <machimpyo@gmail.com>
2024-12-06 14:44:11 +0900

    project name 변경

---
+++ workspace/Portable/P101.AMDS_Portable_first_creation_on_Prototype_Board/Debug/User/udp_protocol.d
@@ -0,0 +1,6 @@
+User/udp_protocol.o: ../User/udp_protocol.c ../User/udp_protocol.h \
+ ../User/collect.h \
+ D:/Sunshine/Tut8_AMDS/workspace/Portable/100B-x.AMDS_Portable_first_creating/Lib/bq34z100.h
+../User/udp_protocol.h:
+../User/collect.h:
+D:/Sunshine/Tut8_AMDS/workspace/Portable/100B-x.AMDS_Portable_first_creating/Lib/bq34z100.h:
