828bf5f8ddaf92545d508d23a2d26144de60aa6b
pesora <machimpyo@gmail.com>
2024-12-06 19:50:09 +0900

    status packet 변경

--- workspace/Portable/P101.AMDS_Portable_first_creation_on_Prototype_Board/User/udp_protocol.h
+++ workspace/Portable/P101.AMDS_Portable_first_creation_on_Prototype_Board/User/udp_protocol.h
@@ -53,11 +53,11 @@ typedef struct {
   uint32_t Count;
 
   //Body
-  float Temp1;
-  float Temp2;
-  uint8_t FAN1;
-  uint8_t FAN2;
-  uint8_t PSU;
+  float Temp1;		//Temp1 value
+  float Temp2;		//Temp2 value
+  uint8_t FAN1;		//Fan Ass'y1
+  uint8_t FAN2;		//Fan Ass'y2
+  uint8_t PSU;		//
   uint8_t DP;
   uint8_t Charger;
   uint16_t Bat_SOC;
@@ -66,6 +66,64 @@ typedef struct {
   uint16_t Bat_CycleCnt;
   uint16_t Bat_IntTemp;
 } StatusPacket_t;
+
+//---------------------------------------------
+typedef struct {
+
+  //개별 항목
+  float Temp1;			//Temp1 value
+  float Temp2;			//Temp2 value
+  uint8_t FAN1;			//Fan Ass'y1 bit-wise
+  uint8_t FAN2;			//Fan Ass'y2 bit-wise
+  uint8_t PSU1;			//PSU1
+  uint8_t PSU2;			//PSU2
+
+  //개별항목, LED항목
+  uint8_t DP1;			//DP1 (LED항목 겸용)
+  uint8_t DP2;			//DP2 (LED항목 겸용)
+
+  //LED 항목
+  uint8_t LED_PSU;		//PSU1 + PSU2
+  uint8_t LED_Temp;		//Temp1+ Temp2
+  uint8_t LED_FAN1;		//fan1 + fan2 + fan3
+  uint8_t LED_FAN2;		//fan4 + fan5 + fan6
+
+} ConsoleStatusPacket_t;
+
+typedef struct {
+
+   //개별 항목
+  float Temp1;			//Temp1 value
+  float Temp2;			//Temp2 value
+  uint8_t FANAssy1;		//Fan Ass'y1 bit-wise
+  uint8_t FANAssy2;		//Fan Ass'y2 bit-wise
+  uint8_t PSU1;			//PSU1
+  uint8_t PSU2;			//PSU2
+
+  //개별항목, LED항목
+  uint8_t DP1;			//DP1 (LED항목 겸용)
+  uint8_t DP2;			//DP2 (LED항목 겸용)
+  uint8_t DP3;			//DP3 (LED항목 겸용)
+
+  //LED 항목
+  uint8_t LED_PSU;		//PSU1 + PSU2 + PSU3
+  uint8_t LED_Temp;		//Temp1+ Temp2
+  uint8_t LED_FAN1;		//fan1
+  uint8_t LED_FAN2;		//fan2
+
+ //개별항목, LED항목
+  uint8_t Charger;		//Charger
+  uint16_t Bat_SOC;		//Bat SOC / 3-digit FND Display
+
+  //개별항목(Non-Display)
+  uint16_t Bat_Volt;
+  uint16_t Bat_Current;
+  uint16_t Bat_CycleCnt;
+  uint16_t Bat_Temp;
+
+} PortableStatusPacket_t;
+
+//---------------------------------------------
 
 extern RequsetPacket_t request;
 extern ReplyPacket_t reply;
