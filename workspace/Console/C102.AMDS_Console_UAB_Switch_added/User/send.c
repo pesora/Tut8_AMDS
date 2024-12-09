39b70293759dd5e758ff43f582501ce49fb693b5
pesora <machimpyo@gmail.com>
2024-12-09 18:22:55 +0900

    24.12.09

---
+++ workspace/Console/C102.AMDS_Console_UAB_Switch_added/User/send.c
@@ -0,0 +1,19 @@
+/*
+ * send.c
+ *
+ *  Created on: Aug 6, 2024
+ *      Author: machi
+ */
+
+
+#include "send.h"
+#include "udp_protocol.h"
+#include "udp_server.h"
+
+void Send_Console_Status(void)
+{
+	StatusCount++;
+	UDP_Make_StatusPacket();
+	udp_send_multicast(&status);
+	print_status_packet(&status);
+}
---
+++ workspace/Console/C102.AMDS_Console_UAB_Switch_added/User/send.h
@@ -0,0 +1,15 @@
+/*
+ * send.h
+ *
+ *  Created on: Aug 6, 2024
+ *      Author: machi
+ */
+
+#ifndef SEND_H_
+#define SEND_H_
+
+
+void Send_Console_Status(void);
+
+
+#endif /* SEND_H_ */
